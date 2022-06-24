ba706dbf1ab49bab9d05e934bfa57ed3f1b96cb7
lucasbrandao <brandslucas@gmail.com>
2022-06-24 15:25:55 -0300

    envia mensagem para o bd quando usuario loga

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express, { json } from "express"
 import  cors  from "cors"
 import { MongoClient } from "mongodb";
 import Joi from "joi";
+import dayjs from "dayjs";
 
 const app = express()
+const hora = dayjs().locale('pt-br')
 
 app.use(cors());
 app.use(json());
@@ -14,6 +16,7 @@ let db;
 mongoClient.connect().then(() => {
 	db = mongoClient.db("banco_api_uol");
 });
+
 
 app.get('/participants', async (req, res) => {
   try{
@@ -48,15 +51,25 @@ app.post("/participants", async (req, res)=> {
         return;
       }
 
-    await db.collection("users").insertOne(
+      await db.collection("users").insertOne(
         {
           name: req.body.name ,
           lastStatus: data
         });
+      await db.collection("messages").insertOne(
+        {
+          from: req.body.name,
+          to: 'Todos',
+          text: 'entra na sala...',
+          type: 'status',
+          time: hora.format('HH:mm:ss')
+        }
+      );
       res.sendStatus(201);
     }catch (error){
       res.sendStatus(error);
   }
 });
+
 
 app.listen(5000 ,  () => console.log('server running - port 5000'));
