3983698411a47fa309c49a69a0775c89f5b07a74
lucasbrandao <brandslucas@gmail.com>
2022-06-24 19:45:07 -0300

    valida mensagem e envia para o bd

--- api/index.js
+++ api/index.js
@@ -17,7 +17,6 @@ mongoClient.connect().then(() => {
 	db = mongoClient.db("banco_api_uol");
 });
 
-
 app.get('/participants', async (req, res) => {
   try{
     const users = await db.collection("users").find({}).toArray();
@@ -33,13 +32,12 @@ app.post("/participants", async (req, res)=> {
       name: Joi.string().min(1).required()
     });
 
-    const userValidade = userSchema.validate(req.body)
-    const {error} = userValidade
+    const userValidation = userSchema.validate(req.body)
+    const {error} = userValidation
     const data = Date.now()
 
     if (error){
       const errorMsgs = error.details.map(err => err.message)
-      console.log(error)
       res.status(422).send(errorMsgs)
       return;
     }
@@ -71,5 +69,55 @@ app.post("/participants", async (req, res)=> {
   }
 });
 
+app.post('/messages', async(req, res) => {
+
+    const sender = req.headers.user;
+    const message = req.body;
+    const messageSchema = Joi.object(
+      {
+        to: Joi.string().min(1).required(),
+        text: Joi.string().min(1).required(),
+        type: Joi.string().valid('message','private_message').required()
+      });
+    
+    const messageValidation = messageSchema.validate(message, { abortEarly: false })
+    const {error} = messageValidation
+
+    if (error){
+      const errorMsgs = error.details.map(err => err.message)
+      res.status(422).send(errorMsgs)
+      return;
+    }
+
+    try {
+      const registeredUser = await db.collection("users").findOne({name : sender})
+      if (!registeredUser){
+        res.send("Usuário não existe, efetue seu cadastro novamente")
+        return;
+      }
+
+      await db.collection("messages").insertOne(
+          {
+            from: sender,
+            to: message.to,
+            text: message.text,
+            type: message.type,
+            time: hora.format('HH:mm:ss')
+          }
+        );
+      res.sendStatus(201)
+    }catch (error){
+      res.sendStatus(error);
+  }
+})
+
+app.get('/messages', async (req, res) => {
+  try{
+    const messages = await db.collection("messages").find({}).toArray();
+    res.send(messages);
+  }catch(error){
+    res.sendStatus(500);
+  }
+});
 
 app.listen(5000 ,  () => console.log('server running - port 5000'));
